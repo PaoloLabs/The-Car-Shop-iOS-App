8915fa01e03e78f0b029a6fdf098f2ffbb8654d8
Paolo <paololabsplus@gmail.com>
2020-08-11 22:22:17 -0400

    Adding Initial Configuration for the project

---
+++ The Car Shop/The Car Shop/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  The Car Shop
+//
+//  Created by Paolo Ramos Mendez on 8/11/20.
+//  Copyright © 2020 paololabs. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+    }
+
+
+}
+
---
+++ The Car Shop/The Car ShopTests/The_Car_ShopTests.swift
@@ -0,0 +1,34 @@
+//
+//  The_Car_ShopTests.swift
+//  The Car ShopTests
+//
+//  Created by Paolo Ramos Mendez on 8/11/20.
+//  Copyright © 2020 paololabs. All rights reserved.
+//
+
+import XCTest
+@testable import The_Car_Shop
+
+class The_Car_ShopTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
